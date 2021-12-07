ebbae562e0db252c5f07a40ae935395071728489
antoninaorobets <pani.antonina.don@gmail.com>
2021-12-07 10:40:17 -0500

    commit tree 5a041114bc050d7621ce0690bca70e1e67058e63

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 const init = () => {
-  
+
+
+    document.querySelector('form').addEventListener('submit', submitFunction);
 }
 
 document.addEventListener('DOMContentLoaded', init);
+
+
+
+function submitFunction(event) {
+    event.preventDefault()
+    const inputValue = document.querySelector('input#searchByID').value
+
+    fetch(`http://localhost:3000/movies/${inputValue}`)
+        .then(rewsponce => rewsponce.json())
+        .then(displaySummary)
+}
+
+function displaySummary(data){
+    document.querySelector('section#movieDetails h4').textContent = data.title
+
+    document.querySelector('#movieDetails p').textContent = data.summary
+
+}
